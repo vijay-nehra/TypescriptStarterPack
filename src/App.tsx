562bbfc83fe44d937b3b97ab1c035476b30a592e
vijayapi <vijayn.apidots@gmail.com>
2025-01-15 23:44:32 +0530

    Shadcn ui setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input"
 function App() {
 
   return (
     <>
-      <p className="text-yellow-400">Vite + React</p>
+      <p className="text-yellow-400">Vite + React + typescript + tailwind + shadcn ui</p>
+        <div className="w-1/2 border border-2">
+            <Input type="email" placeholder="Email" />
+            <Button>Click me</Button>
+        </div>
     </>
   )
 }
